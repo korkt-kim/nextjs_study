b6d6ac94b9917c8e134edcb735226083319a34c5
korkt-kim <gnscott6@naver.com>
2021-08-01 18:53:20 +0900

    lesson 3
    - server-side rendering
    - dynamic rendering (<Link>, next/router, [path].js)

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
+//굳이 서버사이드 렌더링이 필요해보이지 않음
 import axios from 'axios';
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
 import ItemList from '../src/component/ItemList';
+import { Loader } from 'semantic-ui-react';
+
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const API_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
 
   function getData() {
     axios.get(API_URL).then((res) => {
-      console.log(res);
       setList(res.data);
+      setIsLoading(false);
     });
   }
 
@@ -21,18 +25,30 @@ export default function Home() {
     <div>
       <Head>
         <title>korkt-kim</title>
+        <meta name="description" content="홈입니다."></meta>
       </Head>
-      create next app
-      <br />
-      1.컴파일과 번들링이 자동으로 된다.(webpack과 babel)
-      <br />
-      2. 핫로딩이 된다
-      <br />
-      3. 서버사이드 렌더링이 지원된다
-      <br />
-      4. 스태틱 파일을 지원한다.
-      <br />- public 폴더 밑에 만들면된다.
-      <ItemList list={list}></ItemList>
+      {isLoading && (
+        <div style={{ padding: '300px 0' }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <div>
+          create next app
+          <br />
+          1.컴파일과 번들링이 자동으로 된다.(webpack과 babel)
+          <br />
+          2. 핫로딩이 된다
+          <br />
+          3. 서버사이드 렌더링이 지원된다
+          <br />
+          4. 스태틱 파일을 지원한다.
+          <br />- public 폴더 밑에 만들면된다.
+          <ItemList list={list}></ItemList>
+        </div>
+      )}
     </div>
   );
 }
--- pages/view/[pid].js
+++ pages/view/[pid].js
@@ -1,22 +1,40 @@
+//매번 타이틀과 내용이 바뀌기때문에 SSR이 필요해보임
+//소스를 보면 이 상품에 대한 어떠한 정보도 얻을 수 없다. 껍데기만 pre-render된 상태이기때문
+//getServerSideProps 를 사용하여 ssr을 구현한다.
 import axios from 'axios';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { useEffect } from 'react';
 
-export default function ID() {
-  const router = useRouter();
-  const [product, setProduct] = useState();
-  const { pid } = router.query;
+const PID = ({ item }) => {
+  // title, description이 들어감으로서 검색엔진에서 사용할 수 있다.
+  return (
+    <>
+      <Head>
+        <title>{item.name}</title>
+        <meta name="description" content={item.description}></meta>
+      </Head>
+      {item && (
+        <div>
+          <p>{item?.name}</p>
+          <p>{item?.description}</p>
+        </div>
+      )}
+    </>
+  );
+};
 
-  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${pid}.json`;
-  function getData() {
-    axios.get(API_URL).then((res) => {
-      console.log('asdf');
-      setProduct(res.data);
-    });
-  }
-  useEffect(() => {
-    if (pid && pid > 0) getData();
-  }, [pid]);
+export default PID;
 
-  return <p>{product?.name}</p>;
+export async function getServerSideProps(context) {
+  // context는 params 요청, 응답 쿼리들을 받는다
+  const pid = context.params.pid;
+  const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${pid}.json`;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      item: data,
+    },
+  };
 }
